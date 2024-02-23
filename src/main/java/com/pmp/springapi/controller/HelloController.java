a11d5fc75e9a84e061f566132bee5e766468e0ce
preetham-p-m <preethampm1@gmail.com>
2024-02-23 20:15:00 +0530

    get properties from config

--- src/main/java/com/pmp/springapi/controller/HelloController.java
+++ src/main/java/com/pmp/springapi/controller/HelloController.java
@@ -4,13 +4,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pmp.springapi.dto.HelloDto;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@PropertySource(value = "myproperties.properties", ignoreResourceNotFound = true)
 public class HelloController {
 
-    @GetMapping("/")
-    public HelloDto hello() {
-        return new HelloDto("Welcome to Spring Boot Application");
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    @Value("${welcome.message.from.custom.file}")
+    private String welcomeMessageFromCustomFile;
+
+    @GetMapping("/v1")
+    public HelloDto helloOne() {
+        return new HelloDto(welcomeMessage);
+    }
+
+    @GetMapping("/v2")
+    public HelloDto helloTwo() {
+        return new HelloDto(welcomeMessageFromCustomFile);
     }
 }
