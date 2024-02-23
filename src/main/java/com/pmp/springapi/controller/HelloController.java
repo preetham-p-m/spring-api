8e59d3ad940a605ee20aacf1bc638acb6ebad7e3
preetham-p-m <preethampm1@gmail.com>
2024-02-24 00:06:23 +0530

    yml profiles

--- src/main/java/com/pmp/springapi/controller/HelloController.java
+++ src/main/java/com/pmp/springapi/controller/HelloController.java
@@ -5,26 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pmp.springapi.dto.HelloDto;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@PropertySource(value = "myproperties.properties", ignoreResourceNotFound = true)
 public class HelloController {
 
     @Value("${welcome.message}")
     private String welcomeMessage;
 
-    @Value("${welcome.message.from.custom.file}")
-    private String welcomeMessageFromCustomFile;
-
-    @GetMapping("/v1")
+    @GetMapping("/health")
     public HelloDto helloOne() {
         return new HelloDto(welcomeMessage);
-    }
-
-    @GetMapping("/v2")
-    public HelloDto helloTwo() {
-        return new HelloDto(welcomeMessageFromCustomFile);
     }
 }
